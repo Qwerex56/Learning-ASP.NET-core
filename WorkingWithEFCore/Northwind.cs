7c7f074a58be696e834945fef3bd99a47e48ee64
Adrian Czubaty <adriancz.prywatny@gmail.com>
2023-07-06 23:38:45 +0200

    Setup EFCore
    Querying EF Core models
    
    clean branch workspace

---
+++ WorkingWithEFCore/Northwind.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using static System.Console;
+
+namespace Packt.Shared;
+
+public class Northwind : DbContext
+{
+  public DbSet<Category>? Categories { get; set; }
+  public DbSet<Product>? Products { get; set; }
+  
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    base.OnConfiguring(optionsBuilder);
+    if (ProjectConstants.DataBaseProvider == "SQLite") {
+      string path = Path.Combine(Environment.CurrentDirectory, "Northwind.db");
+      WriteLine($"using {path} database file");
+      optionsBuilder.UseSqlite($"Filename={path}");
+    }
+  }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.Entity<Category>()
+      .Property(category => category.CategoryName)
+      .IsRequired()
+      .HasMaxLength(15);
+    if (ProjectConstants.DataBaseProvider == "SQLite") {
+      modelBuilder.Entity<Product>()
+        .Property(product => product.Cost)
+        .HasConversion<double>();
+    }
+  }
+}
