1e120001af8abc0c81533ca6b7170eda7d823e24
Adrian Czubaty <adriancz.prywatny@gmail.com>
2023-07-06 23:38:45 +0200

    Setup EFCore
    Querying EF Core models
    
    clean branch workspace

---
+++ WorkingWithEFCore/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Packt.Shared;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"Using {ProjectConstants.DataBaseProvider}");
+
+static void QueryingCategories() {
+  using (Northwind db = new()) {
+    Console.WriteLine("Categories and how many products they have:");
+    IQueryable<Category>? categories = db.Categories?.Include(c => c.Products);
+
+    if ((categories is null) || (!categories.Any())) {
+      return;
+    }
+     foreach (var c in categories) {
+      Console.WriteLine($"{c.CategoryName} has {c.Products.Count} products.");
+    }
+  }
+}
+
+QueryingCategories();
