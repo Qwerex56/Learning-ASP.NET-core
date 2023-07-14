d265171ef663b795a7b5c3be4e7670ac9a4b7b58
Adrian Czubaty <adriancz.prywatny@gmail.com>
2023-07-14 22:00:37 +0200

    Final Courses and student project

--- CoursesAndStudents/Program.cs
+++ CoursesAndStudents/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using CoursesAndStudents;
+
+using static System.Console;
+
+using (Academy a = new()) {
+  bool deleted = await a.Database.EnsureDeletedAsync();
+  WriteLine($"Databse deleted: {deleted}");
+  bool created = await a.Database.EnsureCreatedAsync();
+  WriteLine($"Databse created: {created}");
+
+  WriteLine("SQL script used to create db: ");
+
+  foreach (var s in a.Students.Include(s => s.Courses)) {
+    WriteLine("{0} {1} attends the following {2} courses:",
+      s.FirstName, s.LastName, s.Courses.Count);
+    foreach (var c in s.Courses) {
+      WriteLine($"  {c.Title}");
+    }
+  }
+}
